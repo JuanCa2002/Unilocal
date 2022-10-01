52c83553cb6cd9304eeb42aaf7dfd078ffa0a0e3
AlejoLGG <luisalejo866@gmail.com>
2022-09-30 21:08:54 -0500

    Haciendo la parte visual de detalles moderador

---
+++ app/src/main/java/com/example/unilocal/activities/DatallesModeradorActivity.kt
@@ -0,0 +1,11 @@
+package com.example.unilocal.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DatallesModeradorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_datalles_moderador)
+    }
+}
