b70d4fd84f147894fe7522ad06fdad0040e29989
AlejoLGG <luisalejo866@gmail.com>
2022-10-08 17:37:36 -0500

    Agregando cambios al detalles lugar y haciendo parte fisica detalles usuario

---
+++ app/src/main/java/com/example/unilocal/activities/DetallesUsuarioActivity.kt
@@ -0,0 +1,11 @@
+package com.example.unilocal.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetallesUsuarioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalles_usuario)
+    }
+}
